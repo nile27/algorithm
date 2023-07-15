5a91c0d334e5efe81291c4d33f63d78577f72d91
nile27 <ddj03104@gmail.com>
2023-07-15 17:52:15 +0900

    백준 2252 수정

--- 5week/2252.js
+++ 5week/2252.js
@@ -1,25 +1,25 @@
 const fs = require("fs");
 const input = fs
-  .readFileSync("../input.txt")
+  .readFileSync("dev/stdin")
   .toString()
   .trim()
   .split("\n")
   .map((item) => item.split(" "));
 const shift = input.shift().map(Number);
 let indegree = Array.from({ length: shift[0] }, () => []);
-let countArr = Array(Number(shift[0])).fill(-1);
+let countArr = Array(Number(shift[0])).fill(0);
 let ans = [];
 
-function answersort(count) {
+function answersort() {
   let quene = [];
   countArr.map((item, idx) => {
-    return item === count ? quene.push(idx + 1) : null;
+    return item === 0 ? quene.push(idx + 1) : null;
   });
   while (quene.length) {
     let zero = quene.shift();
     indegree[zero - 1].forEach((item) => {
       countArr[item - 1]--;
-      countArr[item - 1] === count ? quene.push(item) : null;
+      countArr[item - 1] === 0 ? quene.push(item) : null;
     });
     ans.push(zero);
   }
@@ -27,9 +27,9 @@ function answersort(count) {
 
 for (let i = 0; i < input.length; i++) {
   indegree[Number(input[i][0]) - 1].push(Number(input[i][1]));
-  countArr[Number(input[i][1]) - 1] === -1
-    ? (countArr[Number(input[i][1]) - 1] += 2)
-    : countArr[Number(input[i][1]) - 1]++;
+  countArr[Number(input[i][1]) - 1]++;
 }
+
+answersort();
 
 console.log(ans.join(" "));
