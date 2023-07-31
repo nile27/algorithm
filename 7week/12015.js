29669d0d53546b171fa97c36ad6fb7d2e0e81db0
nile27 <ddj03104@gmail.com>
2023-07-31 10:07:32 +0900

    백준 12015.js

---
+++ 7week/12015.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const input = fs.readFileSync("../input.txt").toString().trim().split("\n");
+
+// let arr = input[1].split(" ").map(Number);
+// let dp = [];
+
+// function search(start, end, n) {
+//   if (start >= end) return end;
+
+//   let mid = parseInt((start + end) / 2);
+
+//   if (dp[mid] < n) return search(mid + 1, end, n);
+//   else return search(start, mid, n);
+// }
+
+// dp.push(arr[0]);
+
+// for (let i = 1; i < arr.length; i++) {
+//   if (dp[dp.length - 1] < arr[i]) {
+//     dp.push(arr[i]);
+//   } else {
+//     let idx = search(0, dp.length - 1, arr[i]);
+//     dp[idx] = arr[i];
+//   }
+// }
+// console.log(dp.length);
+
+let arr = input[1].split(" ").map(Number);
+let dp = [];
+
+function search(start, end, n) {
+  let mid = 0;
+
+  while (start < end) {
+    mid = parseInt((start + end) / 2);
+    dp[mid] < n ? (start = mid + 1) : (end = mid);
+  }
+  return end;
+}
+
+dp.push(arr[0]);
+
+for (let i = 1; i < arr.length; i++) {
+  if (dp[dp.length - 1] < arr[i]) {
+    dp.push(arr[i]);
+  } else {
+    let idx = search(0, dp.length - 1, arr[i]);
+    dp[idx] = arr[i];
+  }
+}
+console.log(dp.length);
