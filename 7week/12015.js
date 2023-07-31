3779dd79b26e8fd427f74ce49bb63b2684bafbd5
nile27 <ddj03104@gmail.com>
2023-07-31 11:11:36 +0900

    Readme 추가

--- 7week/12015.js
+++ 7week/12015.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const input = fs.readFileSync("../input.txt").toString().trim().split("\n");
+// const fs = require("fs");
+// const input = fs.readFileSync("../input.txt").toString().trim().split("\n");
 
 // let arr = input[1].split(" ").map(Number);
 // let dp = [];
