17efc9cb3dbd4db207fd713e01974c11447e1056
nile27 <ddj03104@gmail.com>
2023-08-04 17:12:41 +0900

    백준 1021

---
+++ 8week/1021.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync("../input.txt").toString().trim().split("\n").map((item) => item.replace("\r",'').split(' '));
+const [N, M] = input.shift().map(Number);
+const arr = input[0].map(item => Number(item));
+let queue = Array.from ( {length: N}, (_,i) => i+1);
+let [ans,mid] = [0,0];
+
+for(let i = 0; i < M ; i++){
+    mid = parseInt(queue.length / 2);
+    if(mid < queue.findIndex(item => arr[i] === item)){
+        while(arr[i] !== queue[0]){
+            queue.unshift(queue.pop());
+            ans++;
+        }
+    }
+    else{
+        while(arr[i] !== queue[0]){
+            queue.push(queue.shift());
+            ans++;
+        }
+    }
+
+    queue.shift();
+}
+console.log(ans)
+
+
+
+
