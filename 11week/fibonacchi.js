8a1f68791d1d433d42c063f9ed872d7b957573b9
nile27 <ddj03104@gmail.com>
2023-09-12 15:44:18 +0900

    프로그래머스 비포나치 수 수정

--- 11week/fibonacchi.js
+++ 11week/fibonacchi.js
@@ -9,15 +9,15 @@ function solution(n) {
 
 // 정답 코드
 function solution(n) {
-  var answer = [];
-  for (var i = 0; i <= n; i++) {
+  let answer = [];
+  for (let i = 0; i <= n; i++) {
     if (i == 0) answer.push(0);
     if (i == 1) answer.push(1);
     if (i >= 2) {
-      var sum = answer[i - 1] + answer[i - 2];
+      let sum = answer[i - 1] + answer[i - 2];
       answer.push(sum % 1234567);
     }
   }
-  var result = answer[n] % 1234567;
+  let result = answer[n] % 1234567;
   return result;
 }
