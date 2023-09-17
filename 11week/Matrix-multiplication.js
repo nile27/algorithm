de29ad57c413b3b381f0ae1d9afcf31a2b84e76b
nile27 <ddj03104@gmail.com>
2023-09-17 17:25:58 +0900

    프로그래머스 행렬 곱셈

---
+++ 11week/Matrix-multiplication.js
@@ -0,0 +1,18 @@
+function solution(arr1, arr2) {
+  let answer = [];
+  let sumArr = [];
+  let sum = 0;
+
+  for (let k = 0; k < arr1.length; k++) {
+    for (let i = 0; i < arr2[0].length; i++) {
+      for (let j = 0; j < arr2.length; j++) {
+        sum = sum + arr1[k][j] * arr2[j][i];
+      }
+      sumArr.push(sum);
+      sum = 0;
+    }
+    answer.push(sumArr);
+    sumArr = [];
+  }
+  return answer;
+}
