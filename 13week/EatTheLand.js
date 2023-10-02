9acb9c8c11ecb9e4351853511fa794371f081a53
nile27 <ddj03104@gmail.com>
2023-10-02 15:57:22 +0900

    프로그래머스 땅따먹기

---
+++ 13week/EatTheLand.js
@@ -0,0 +1,16 @@
+//프로그래머스 땅따먹기
+
+function solution(land) {
+  let answer = [];
+  let trigger = [0, 1, 2, 3];
+
+  for (let i = 1; i < land.length; i++) {
+    land[i][0] += Math.max(land[i - 1][1], land[i - 1][2], land[i - 1][3]);
+    land[i][1] += Math.max(land[i - 1][0], land[i - 1][2], land[i - 1][3]);
+    land[i][2] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][3]);
+    land[i][3] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][2]);
+  }
+  answer = land[land.length - 1];
+
+  return Math.max(...answer);
+}
