1b60d336053c7adb78e2e1293022d1d1f40fa895
nile27 <ddj03104@gmail.com>
2023-10-20 22:51:46 +0900

    프로그래머스 가장 가까운 같은 글자

---
+++ 15week/nearlyStr.js
@@ -0,0 +1,16 @@
+// 프로그래머스 가장 가까운 같은 글자
+function solution(s) {
+  var answer = [];
+  let table = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (table[s[i]] === undefined) {
+      table[s[i]] = i;
+      answer.push(-1);
+    } else {
+      answer.push(i - table[s[i]]);
+      table[s[i]] = i;
+    }
+  }
+  return answer;
+}
