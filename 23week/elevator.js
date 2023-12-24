f3e7c5f4a79d63a2300bdae1c3c8525ca3deaa36
nile27 <ddj03104@gmail.com>
2023-12-24 16:22:01 +0900

    프로그래머스 마법의 엘리베이터

--- 23week/elevator.js
+++ 23week/elevator.js
@@ -1,24 +1,18 @@
+// 프로그래머스 마법의 엘리베이터
 function solution(storey) {
   var answer = 0;
-  let [single, double] = [0, 0];
-  let num = String(storey).length;
-  let numArr = Array.from({ length: num }, (_, i) => 10 ** i);
+  let storeyArr = String(storey).split("").map(Number).reverse();
 
-  for (let i = num - 1; i > -1; i--) {
-    single = Math.abs(storey % numArr[i]);
-    double = Math.abs(single - numArr[i]);
-
-    if (single <= double) {
-      answer += parseInt(storey / numArr[i]);
-      storey -= parseInt(storey / numArr[i]) * numArr[i];
+  for (let i = 0; i < storeyArr.length; i++) {
+    if (storeyArr[i] < 5) answer += storeyArr[i];
+    else if (storeyArr[i] === 5) {
+      answer += 5;
+      if (storeyArr[i + 1] && storeyArr[i + 1] >= 5) storeyArr[i + 1]++;
     } else {
-      answer += parseInt(storey / numArr[i]) + 1;
-      storey -= (parseInt(storey / numArr[i]) + 1) * numArr[i];
+      answer += 10 - storeyArr[i];
+      if (storeyArr[i + 1] || storeyArr[i + 1] === 0) storeyArr[i + 1]++;
+      if (i === storeyArr.length - 1) answer += 1;
     }
-    storey = Math.abs(storey);
-
-    console.log(single, double, answer, storey, numArr[i]);
   }
-
   return answer;
 }
