5c6e4fd6c7b4a40b219941383a831a4a3315c0ff
nile27 <ddj03104@gmail.com>
2023-12-25 20:57:23 +0900

    프로그래머스 옹알이(2)

---
+++ 24week/babbling.js
@@ -0,0 +1,24 @@
+// 프로그래머스 옹알이(2)
+function solution(babbling) {
+  var answer = 0;
+  let bab = ["aya", "ye", "woo", "ma"];
+  let stack = "";
+  let index = 0;
+  let prev = "";
+
+  for (let item of babbling) {
+    for (let i = 0; i < item.length; i++) {
+      stack += item[i];
+      index = bab.indexOf(stack);
+      if (index !== -1 && stack !== prev) {
+        prev = stack;
+        stack = "";
+      }
+    }
+    if (stack.length === 0) answer++;
+    stack = "";
+    prev = "";
+  }
+
+  return answer;
+}
