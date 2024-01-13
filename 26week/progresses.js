7648436d36e52071ede8c920143bc4ba55a4fd4d
nile27 <ddj03104@gmail.com>
2024-01-13 09:50:16 +0900

    프로그래머스 기능개발

---
+++ 26week/progresses.js
@@ -0,0 +1,26 @@
+// 프로그래머스 기능개발
+function solution(progresses, speeds) {
+  var answer = [];
+  let count = 0;
+
+  while (progresses.length !== 0) {
+    if (progresses[0] >= 100) {
+      progresses.shift();
+      speeds.shift();
+      count += 1;
+      while (progresses[0] >= 100) {
+        count += 1;
+        progresses.shift();
+        speeds.shift();
+      }
+      answer.push(count);
+      count = 0;
+    } else {
+      for (let i = 0; i < progresses.length; i++) {
+        progresses[i] = progresses[i] + speeds[i];
+      }
+    }
+  }
+
+  return answer;
+}
