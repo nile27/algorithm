8016658d658b07342419a4187538c7aaa042acff
nile27 <ddj03104@gmail.com>
2024-01-29 14:45:45 +0900

    프로그래머스 할 일 목록

---
+++ 29week/todo_list.js
@@ -0,0 +1,8 @@
+// 프로그래머스 할 일 목록
+function solution(todo_list, finished) {
+  var answer = [];
+  for (let i = 0; i < todo_list.length; i++) {
+    !finished[i] ? answer.push(todo_list[i]) : null;
+  }
+  return answer;
+}
