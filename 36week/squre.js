27c509c3e8837bc5afcb3b9d91716b1430a8a135
nile27 <ddj03104@gmail.com>
2024-04-26 14:48:08 +0900

    프로그래머스 직사각형

---
+++ 36week/squre.js
@@ -0,0 +1,9 @@
+function solution(dots) {
+  dots.sort((a, b) => a[0] - b[0]);
+  let [x, y] = [
+    Math.abs(dots[0][1] - dots[1][1]),
+    Math.abs(dots[1][0] - dots[2][0]),
+  ];
+
+  return Math.abs(x * y);
+}
