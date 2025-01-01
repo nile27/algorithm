cb80e6ef46c3f0c143c41d177ae7c7e2c853ac0d
nile27 <114140840+nile27@users.noreply.github.com>
2025-01-01 17:43:02 +0900

    Create 1024.js

---
+++ 63week/1024.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+const N = BigInt(input[0]);
+const L = Number(input[1]);
+
+let x = -1n;
+let temp = 0n;
+let result = [];
+let i = L;
+
+for (; i <= 100; i++) {
+  temp = N - BigInt(i * (i - 1)) / 2n;
+  if (temp < 0n) break;
+  if (temp % BigInt(i) === 0n) {
+    x = temp / BigInt(i);
+    if (x >= 0n) break;
+  }
+}
+
+if (x === -1n || x < 0n) {
+  console.log(-1);
+} else {
+  result = Array.from({ length: i }, (_, idx) => (x + BigInt(idx)).toString());
+  console.log(result.join(" "));
+}
