c29c74a43bde9f62d6d46eb10b85e70611af704a
nile27 <114140840+nile27@users.noreply.github.com>
2025-04-25 12:23:43 +0900

    Create 택배 상자 꺼내기.js

---
+++ 79week/택배 상자 꺼내기.js
@@ -0,0 +1,27 @@
+function solution(n, w, num) {
+  let answer = 1;
+  let boxArr = Array.from({ length: Math.ceil(n / w) }, (_, i) =>
+    Array.from({ length: w }, (_, j) => {
+      if (i % 2 === 0) {
+        return i * w + j + 1;
+      } else {
+        return (i + 1) * w - j;
+      }
+    })
+  );
+  let idx = 0;
+
+  for (let i = 0; i < boxArr.length; i++) {
+    idx = boxArr[i].findIndex((x) => x === num);
+    console.log(idx);
+    if (idx !== -1) {
+      break;
+    }
+  }
+
+  boxArr.forEach((item) => {
+    if (item[idx] > num && item[idx] <= n) answer++;
+  });
+
+  return answer;
+}
