df671c346734da19658eb7443de8a8305782807e
nile27 <114140840+nile27@users.noreply.github.com>
2025-09-20 15:51:37 +0900

    Create 11568.js

---
+++ DP/11568.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const n = Number(input[0]);
+const arr = input[1].split(" ").map(Number);
+
+let dp = Array.from({ length: n }, () => 1);
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+        if (arr[i] > arr[j]) {
+            dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+    }
+}
+
+console.log(Math.max(...dp));
